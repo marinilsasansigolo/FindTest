a0b3052fbe0bb26abd42b8a0a60730db950b7453
Marinilsa Sansigolo <marisr7@hotmail.com>
2021-10-10 10:50:48 -0300

    Versao inicial.

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from  'react';
+import {View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text} from 'react-native';
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <KeyboardAvoidingView style = {styles.background}>
+    <View style = {styles.containerLogo}>
+    <Image
+     source={require('./src/assets/logo.png')}
+     />
+   </View>
+    
+      <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        autocorrect={False}
+        onChangedText = {()=>{}}
+      />
+      </View>
+
+      <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        autocorrect={False}
+        onChangedText = {()=>{}}
+      />
+
+      <TouchableOpacity style={styles.btnSubmit}>
+        <Text style={styles.btnSubmit}> Acessar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btnRegister}>
+        <Text style={styles.registerText}>Criar Conta</Text>
+      </TouchableOpacity>
+
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    const styles = StyleSheet.create({
+      background:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#08B6CB'
+      },
+
+      containerLogo:{
+        flex:1,
+        justifyContent: 'center',
+      
+      },
+
+      container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        paddingBottom: 50
+      },
+
+      input:{
+        backgroundColor: '#FFF',
+        width: '90%',
+        marginBottom: 15,
+        color: '#222',
+        fontSize: 17,
+        borderRadius: 7,
+        padding: 10,
+      },
+
+      btnSubmit:{
+        backgroundColor:'#35AAFF',
+        width: '90%',
+        heigth: 45,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 7
+      },
+
+  submitText:{
+    color: '#FFF',
+    fontSize:18
   },
+
+  btnRegister:{
+    marginTop: 10,
+  },
+
+  registerText:{
+    color:'#FFF'
+  }
+
+
 });
